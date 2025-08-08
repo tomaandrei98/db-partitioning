d84d48ffb38fb549e235e8f954c8d30662e2321e
Toma <tomaandrei98@gmail.com>
2025-08-08 14:26:00 +0300

    added initial configuration

---
+++ src/main/java/com/example/partitioning/PartitioningApplication.java
@@ -0,0 +1,13 @@
+package com.example.partitioning;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PartitioningApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PartitioningApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/partitioning/PartitioningApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.partitioning;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PartitioningApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
